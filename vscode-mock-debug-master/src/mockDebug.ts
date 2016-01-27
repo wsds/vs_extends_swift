0c9dc08fb1bb4a597e9de9bd88295065585e0f02
wsds <wsds888@163.com>
2016-01-28 00:07:18 +0800

    vs extends including: theme/ language highlight/ debugger.

---
+++ vscode-mock-debug-master/src/mockDebug.ts
@@ -0,0 +1,255 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
+import {DebugSession, InitializedEvent, TerminatedEvent, StoppedEvent, OutputEvent, Thread, StackFrame, Scope, Source} from './common/debugSession';
+import {Handles} from './common/handles';
+import {readFileSync} from 'fs';
+import {basename, dirname} from 'path';
+
+var WebSocket = require('ws');
+var ws = new WebSocket("ws://112.126.71.175:8092/");
+/**
+ * This interface should always match the schema found in the mock-debug extension manifest.
+ */
+export interface LaunchRequestArguments {
+	/** An absolute path to the program to debug. */
+	program: string;
+	/** Automatically stop target after launch. If not specified, target does not stop. */
+	stopOnEntry?: boolean;
+}
+
+class MockDebugSession extends DebugSession {
+
+	// we don't support multiple threads, so we can use a hardcoded ID for the default thread
+	private static THREAD_ID = 1;
+
+	private __currentLine: number;
+	private get _currentLine(): number {
+        return this.__currentLine;
+    }
+
+	private set _currentLine(line: number) {
+        this.__currentLine = line;
+		this.sendEvent(new OutputEvent(`line 101 to: ${line}\n`));	// print current line on debug console
+
+	}
+
+	private _sourceFile: string;
+	private _sourceLines: string[];
+	private _breakPoints: any;
+	private _variableHandles: Handles<string>;
+
+
+	public constructor(debuggerLinesStartAt1: boolean, isServer: boolean = false) {
+		super(debuggerLinesStartAt1, isServer);
+		this._sourceFile = null;
+		this._sourceLines = [];
+		this._currentLine = 0;
+		this._breakPoints = {};
+		this._variableHandles = new Handles<string>();
+	}
+
+	protected initializeRequest(response: DebugProtocol.InitializeResponse, args: DebugProtocol.InitializeRequestArguments): void {
+		this.sendResponse(response);
+
+		// now we are ready to accept breakpoints -> fire the initialized event to give UI a chance to set breakpoints
+		this.sendEvent(new InitializedEvent());
+	}
+
+	protected launchRequest(response: DebugProtocol.LaunchResponse, args: LaunchRequestArguments): void {
+		this._sourceFile = args.program;
+		this._sourceLines = readFileSync(this._sourceFile).toString().split('\n');
+
+		var thiz = this;
+
+		ws.on('open', function open() {
+			console.log('WebSocket connect success!');
+			thiz.sendEvent(new OutputEvent("WebSocket connect success!!!\n"));
+			ws.send("['command':'test']");
+		});
+
+		ws.on('message', function(data, flags) {
+			console.log(data);
+			thiz.sendEvent(new OutputEvent(data));
+		});
+
+		if (args.stopOnEntry) {
+			this._currentLine = 0;
+			this.sendResponse(response);
+
+			// we stop on the first line
+			this.sendEvent(new StoppedEvent("entry", MockDebugSession.THREAD_ID));
+			this.sendEvent(new OutputEvent("Welcome to debug candy."));
+			this.sendEvent(new OutputEvent(this._sourceFile + '\n'));
+			this.sendEvent(new OutputEvent(basename(this._sourceFile) + '\n'));
+			this.sendEvent(new OutputEvent(dirname(this._sourceFile) + '\n'));
+
+
+		} else {
+			// we just start to run until we hit a breakpoint or an exception
+			this.continueRequest(response, { threadId: MockDebugSession.THREAD_ID });
+		}
+	}
+
+	protected setBreakPointsRequest(response: DebugProtocol.SetBreakpointsResponse, args: DebugProtocol.SetBreakpointsArguments): void {
+
+		var path = args.source.path;
+		var clientLines = args.lines;
+
+		// read file contents into array for direct access
+		var lines = readFileSync(path).toString().split('\n');
+
+		var newPositions = [clientLines.length];
+		var breakpoints = [];
+
+		// verify breakpoint locations
+		for (var i = 0; i < clientLines.length; i++) {
+			var l = this.convertClientLineToDebugger(clientLines[i]);
+			var verified = false;
+			if (l < lines.length) {
+				// if a line starts with '+' we don't allow to set a breakpoint but move the breakpoint down
+				if (lines[l].indexOf("+") == 0)
+					l++;
+				// if a line starts with '-' we don't allow to set a breakpoint but move the breakpoint up
+				if (lines[l].indexOf("-") == 0)
+					l--;
+				verified = true;    // this breakpoint has been validated
+			}
+			newPositions[i] = l;
+			breakpoints.push({ verified: verified, line: this.convertDebuggerLineToClient(l) });
+		}
+		this._breakPoints[path] = newPositions;
+
+		// send back the actual breakpoints
+		response.body = {
+			breakpoints: breakpoints
+		};
+		this.sendResponse(response);
+	}
+
+	protected threadsRequest(response: DebugProtocol.ThreadsResponse): void {
+
+		// return the default thread
+		response.body = {
+			threads: [
+				new Thread(MockDebugSession.THREAD_ID, "thread 1")
+			]
+		};
+		this.sendResponse(response);
+	}
+
+	protected stackTraceRequest(response: DebugProtocol.StackTraceResponse, args: DebugProtocol.StackTraceArguments): void {
+
+		const frames = new Array<StackFrame>();
+		const words = this._sourceLines[this._currentLine].trim().split(/\s+/);
+		// create three fake stack frames.
+		for (let i = 0; i < 3; i++) {
+			// use a word of the line as the stackframe name
+			const name = words.length > i ? words[i] : "frame";
+			frames.push(new StackFrame(i, `${name}(${i})`, new Source(basename(this._sourceFile), this.convertDebuggerPathToClient(this._sourceFile)), this.convertDebuggerLineToClient(this._currentLine), 0));
+		}
+		response.body = {
+			stackFrames: frames
+		};
+		this.sendResponse(response);
+	}
+
+	protected scopesRequest(response: DebugProtocol.ScopesResponse, args: DebugProtocol.ScopesArguments): void {
+
+		const frameReference = args.frameId;
+		const scopes = new Array<Scope>();
+		scopes.push(new Scope("Local", this._variableHandles.create("local_" + frameReference), false));
+		scopes.push(new Scope("Closure", this._variableHandles.create("closure_" + frameReference), false));
+		scopes.push(new Scope("Global", this._variableHandles.create("global_" + frameReference), true));
+
+		response.body = {
+			scopes: scopes
+		};
+		this.sendResponse(response);
+	}
+
+	protected variablesRequest(response: DebugProtocol.VariablesResponse, args: DebugProtocol.VariablesArguments): void {
+
+		const variables = [];
+		const id = this._variableHandles.get(args.variablesReference);
+		if (id != null) {
+			variables.push({
+				name: id + "_i",
+				value: "123",
+				variablesReference: 0
+			});
+			variables.push({
+				name: id + "_f",
+				value: "3.14",
+				variablesReference: 0
+			});
+			variables.push({
+				name: id + "_s",
+				value: "hello world",
+				variablesReference: 0
+			});
+			variables.push({
+				name: id + "_o",
+				value: "Object",
+				variablesReference: this._variableHandles.create("object_")
+			});
+		}
+
+		response.body = {
+			variables: variables
+		};
+		this.sendResponse(response);
+	}
+
+	protected continueRequest(response: DebugProtocol.ContinueResponse, args: DebugProtocol.ContinueArguments): void {
+
+		const lines = this._breakPoints[this._sourceFile];
+		for (let ln = this._currentLine + 1; ln < this._sourceLines.length; ln++) {
+			// is breakpoint on this line?
+			if (lines && lines.indexOf(ln) >= 0) {
+				this._currentLine = ln;
+				this.sendResponse(response);
+				this.sendEvent(new StoppedEvent("step", MockDebugSession.THREAD_ID));
+				return;
+			}
+			// if word 'exception' found in source -> throw exception
+			if (this._sourceLines[ln].indexOf("exception") >= 0) {
+				this._currentLine = ln;
+				this.sendResponse(response);
+				this.sendEvent(new StoppedEvent("exception", MockDebugSession.THREAD_ID));
+				this.sendEvent(new OutputEvent(`exception in line: ${ln}\n`, 'stderr'));
+				return;
+			}
+		}
+		this.sendResponse(response);
+		// no more lines: run to end
+		this.sendEvent(new TerminatedEvent());
+	}
+
+	protected nextRequest(response: DebugProtocol.NextResponse, args: DebugProtocol.NextArguments): void {
+
+		for (let ln = this._currentLine + 1; ln < this._sourceLines.length; ln++) {
+			if (this._sourceLines[ln].trim().length > 0) {   // find next non-empty line
+				this._currentLine = ln;
+				ws.send("['command':'test']");
+				this.sendResponse(response);
+				this.sendEvent(new StoppedEvent("step", MockDebugSession.THREAD_ID));
+				return;
+			}
+		}
+		this.sendResponse(response);
+		// no more lines: run to end
+		this.sendEvent(new TerminatedEvent());
+	}
+
+	protected evaluateRequest(response: DebugProtocol.EvaluateResponse, args: DebugProtocol.EvaluateArguments): void {
+		response.body = {
+			result: `evaluate(${args.expression})`,
+			variablesReference: 0
+		};
+		this.sendResponse(response);
+	}
+}
+
+DebugSession.run(MockDebugSession);
